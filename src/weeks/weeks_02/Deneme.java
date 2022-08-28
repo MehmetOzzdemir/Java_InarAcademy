d13794fb65a210d673bfa6c7c5309b3927a7a594
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-08-29 01:29:41 +0300

    Chapter 3'S Checkpoints

--- src/weeks/weeks_02/Deneme.java
+++ src/weeks/weeks_02/Deneme.java
@@ -4,6 +4,16 @@ public class Deneme {
 
 	public static void main(String[] args) {
 		System.out.println("I Love Wooden Spoon");
+		int x=3;
+		int y=2;
+		if (x > 2) {
+			if (y > 2) {
+			 int z = x + y;
+			 System.out.println("z is " + z);
+			 }
+			}
+			else
+			 System.out.println("x is " + x);
 
 	}
 
