9efa483af87dfd87622c39892b76c6548b80ebf0
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-08-30 23:05:16 +0300

    Quiz 18 done

--- src/chapters/chapter_03/Quiz_03_12.java
+++ src/chapters/chapter_03/Quiz_03_12.java
@@ -1,10 +1,22 @@
 package chapters.chapter_03;
 
+import java.util.Scanner;
+
 public class Quiz_03_12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner (System.in);
+		System.out.print("Enter a three digit integer : ");
+		int number =input.nextInt();
+		int digit1 = number /100;
+		int remain = number %100;
+		int digit3 = remain%10;
+		
+		if(digit1==digit3) {
+			System.out.print(number +" is a palindrome ");
+		}else
+			System.out.print(number +" is not a palindrome ");
+		
 	}
 
 }
