f3e80e2bb597708aa668809244f6149b166e7c0e
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-09-20 09:44:01 +0300

    Quiz 14 done

--- src/chapters/chapter_05/Quiz_05_14.java
+++ src/chapters/chapter_05/Quiz_05_14.java
@@ -1,10 +1,25 @@
 package chapters.chapter_05;
 
+import java.util.Scanner;
+
 public class Quiz_05_14 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input =new Scanner (System.in);
+		System.out.print("Enter First Number : ");
+		int number1=input.nextInt();
+		System.out.print("Enter Second Number : ");		
+		int number2=input.nextInt();
+		int d;
+		if(number1<number2) {
+			d=number1;
+		}else {
+			d=number2;
+		}
+		while(number1%d != 0 ||number2%d != 0 ) {
+			d--;
+		}
+		System.out.print("The greatest common divisor for ("+number1+" , "+number2+") : "+d);
 	}
 
 }
