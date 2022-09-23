3622361b04a281e634ddfe10636e9ea5909da4bf
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-09-23 23:36:11 +0300

    Quiz 20 Done

---
+++ src/chapters/chapter_05/Deneme2.java
@@ -0,0 +1,30 @@
+package chapters.chapter_05;
+
+import java.util.Scanner;
+
+public class Deneme2 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int number1 = 0;
+		System.out.println("Enter The Pyramid' Line ");
+		int number2 = input.nextInt();
+		for (int row = 1; row <= 128; row += row) {
+			
+			for (int space = 0; space < number2; space++) {
+				System.out.print("    ");
+			}
+			for (int i = 1; i <= row; i += i) {
+				System.out.printf("%4d", (i));
+			}
+			for (int j = number1; j > 0; j /= 2) {
+				System.out.printf("%4d", (j));
+
+			}
+			System.out.println();
+			number2--;
+			number1 = row;
+		}
+	}
+
+}
