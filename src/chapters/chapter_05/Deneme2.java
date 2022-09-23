65d7045b512afd6fb5204aa031f5744cc2e26726
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-09-24 00:51:03 +0300

    Quiz 21 Done

--- src/chapters/chapter_05/Deneme2.java
+++ src/chapters/chapter_05/Deneme2.java
@@ -7,23 +7,23 @@ public class Deneme2 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int number1 = 0;
-		System.out.println("Enter The Pyramid' Line ");
-		int number2 = input.nextInt();
-		for (int row = 1; row <= 128; row += row) {
-			
-			for (int space = 0; space < number2; space++) {
+		System.out.println("\t\t\t The Pyramid");
+		System.out.println("\t\t\t ____________");
+		System.out.println();
+		int number2 = 7;
+		for (int row=1;row<=128;row+=row) {
+			for(int space=0;space<number2;space++) {
 				System.out.print("    ");
 			}
-			for (int i = 1; i <= row; i += i) {
-				System.out.printf("%4d", (i));
+			for(int i=1;i<=row;i+=i) {
+				System.out.printf("%4d",(i));
 			}
-			for (int j = number1; j > 0; j /= 2) {
-				System.out.printf("%4d", (j));
-
+			for(int j=number1;j>0;j/=2) {
+				System.out.printf("%4d",(j));
 			}
 			System.out.println();
 			number2--;
-			number1 = row;
+			number1=row;
 		}
 	}
 
---
+++ src/chapters/chapter_05/Quiz_05_21.java
@@ -0,0 +1,26 @@
+package chapters.chapter_05;
+
+import java.util.Scanner;
+
+public class Quiz_05_21 {
+
+	public static void main(String[] args) {
+		Scanner input =new Scanner (System.in);
+		System.out.print("Loan Amount : ");
+		int loan=input.nextInt();
+		System.out.print("Number of Years : ");
+		int years=input.nextInt();
+		
+		System.out.println("Interest Rate \t Monthly Paymen \t Total Payment");
+		for(double interestRate=5;interestRate<=8;interestRate+=0.125) {
+			double monthlyInterestRate = interestRate / 1200;
+			System.out.print((interestRate+"% \t\t "));
+			
+			double monthlyPayment = loan * monthlyInterestRate / (1	- 1 / Math.pow(1 + monthlyInterestRate,years * 12));
+			System.out.print(monthlyPayment+" \t ");
+			double totalPayment = monthlyPayment * years * 12;
+			System.out.println(totalPayment);
+		}
+	}
+
+}
