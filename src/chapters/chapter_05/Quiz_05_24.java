22909590ba35adf29433fc3713fe293252895164
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-09-25 01:37:16 +0300

    Quiz 24 Done

--- src/chapters/chapter_05/Quiz_05_24.java
+++ src/chapters/chapter_05/Quiz_05_24.java
@@ -3,8 +3,13 @@ package chapters.chapter_05;
 public class Quiz_05_24 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double j=1.0;
+		double sum=0.0;
+		for(double i=1.0;i<98.0;i+=2.0) {
+			j=j+2;
+			sum += i/j;
+		}
+		System.out.println(sum);
 	}
 
 }
