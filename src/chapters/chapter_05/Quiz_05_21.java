0cf8e81977380050306276b4a9b657de2434f247
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-09-24 11:47:19 +0300

    Quiz 22 Done

--- src/chapters/chapter_05/Quiz_05_21.java
+++ src/chapters/chapter_05/Quiz_05_21.java
@@ -11,15 +11,15 @@ public class Quiz_05_21 {
 		System.out.print("Number of Years : ");
 		int years=input.nextInt();
 		
-		System.out.println("Interest Rate \t Monthly Paymen \t Total Payment");
+		System.out.println("Interest Rate \t Monthly Payment \t Total Payment");
 		for(double interestRate=5;interestRate<=8;interestRate+=0.125) {
 			double monthlyInterestRate = interestRate / 1200;
-			System.out.print((interestRate+"% \t\t "));
+			System.out.print(interestRate+"% \t\t ");
 			
 			double monthlyPayment = loan * monthlyInterestRate / (1	- 1 / Math.pow(1 + monthlyInterestRate,years * 12));
-			System.out.print(monthlyPayment+" \t ");
+			System.out.print((int)(monthlyPayment*100)/100.0+" \t\t ");
 			double totalPayment = monthlyPayment * years * 12;
-			System.out.println(totalPayment);
+			System.out.println((int)(totalPayment*100)/100.0);
 		}
 	}
 
