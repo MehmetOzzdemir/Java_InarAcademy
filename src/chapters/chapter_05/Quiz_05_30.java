ed669e47f874672a81306d7ce3495b68b3e23486
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-09-26 00:13:47 +0300

    Quiz 30 Done

--- src/chapters/chapter_05/Quiz_05_30.java
+++ src/chapters/chapter_05/Quiz_05_30.java
@@ -1,10 +1,26 @@
 package chapters.chapter_05;
 
+import java.util.Scanner;
+
 public class Quiz_05_30 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input =new Scanner (System.in);
+		System.out.print("Enter Monthly Saving Amount : ");
+		double saving=input.nextDouble();
+		System.out.print("Enter Annually Interest Rate (e.g. 5) : ");
+		double annuallyInterestRate = input.nextDouble();
+		System.out.print("Enter Saving Time Number of Month : ");
+		int month = input.nextInt();
+		double monthlyInterestRate = annuallyInterestRate/1200;
+		monthlyInterestRate=(int)(monthlyInterestRate*100000)/100000.;
+		double totalSaving=0;
+		for(int i=0;i<month;i++) {
+			
+			totalSaving=(totalSaving+saving)*(1+monthlyInterestRate);
+			System.out.printf(i+1+" . Month Saving Amount : "+ "$%.3f \n",totalSaving);
+		}
+		System.out.printf("Amount in savings account after " +month + " months: $%.3f",totalSaving);
 	}
 
 }
