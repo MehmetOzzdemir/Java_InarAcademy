8650a95efffda563ec2347007731963d965ad71d
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-09-29 22:47:16 +0300

    Quiz 38 Done

--- src/chapters/chapter_05/Quiz_05_38.java
+++ src/chapters/chapter_05/Quiz_05_38.java
@@ -1,10 +1,19 @@
 package chapters.chapter_05;
 
+import java.util.Scanner;
+
 public class Quiz_05_38 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter The Decimal Integer : ");
+		int decimal = input.nextInt();
 
+		String octal = "";
+		for (int i = decimal; i > 0; i /= 8) {
+			octal = (i % 8) + octal;
+		}
+		System.out.println(decimal + " Decimal : " + octal + " Binary");
 	}
 
 }
